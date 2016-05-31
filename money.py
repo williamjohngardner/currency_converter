d2aba00369541835a0a0f28c351ece5d5a60e1c7
williamjohngardner <bill@williamjohngardner.com>
2016-05-31 14:45:52 -0400

    3

--- money.py
+++ money.py
@@ -3,33 +3,69 @@ class Money:
     def __init__(self, amount, currency):
         self.amount = amount
         self.currency = currency
-        self.usd = 1
-        self.eur = .9
-        self.jpy = 110.65
-        self.btc = .0019
+        self.usd = 'usd'
+        self.eur = 'eur'
+        self.jpy = 'jpy'
+        self.btc = 'btc'
 
     @property
     def currency_type(self):
-        if self.usd:
-            return self.amount * self.usd
-        elif self.eur:
-            return self.amount * self.eur
-        elif self.jpy:
-            return self.amount * self.jpy
+        if self.currency.lower() == self.usd:
+            return self.amount
+        elif self.currency.lower() == self.eur:
+            return self.amount * .9
+        elif self.currency.lower() == self.jpy:
+            return self.amount * 110.65
+        elif self.currency.lower() == self.btc:
+            return self.amount * .0019
         else:
-            return self.amount * self.btc
+            return "Invalid Input"
+
+    def __eq__(self, other):
+        return self.currency_type == other.currency_type
+
+    def __ne__(self, other):
+        return self.currency_type != other.currency_type
 
     def __add__(self, other):
-        return (self.currency_type * self.amount) + (other.currency_type * other.amount)
+        return self.currency_type + other.currency_type
 
     def __mul__(self, other):
-        return Money(self, other)
+        return self.currency_type * other.currency_type
+
+    def __sub__(self, other):
+        return self.currency_type - other.currency_type
 
     def __lt__(self, other):
+        return self.currency_type < other.currency_type
+
+    def __le__(self, other):
+        return self.currency_type <= other.currency_type
+
+    def __gt__(self, other):
         return self.currency_type > other.currency_type
 
+    def __ge__(self, other):
+        return self.currency_type >= other.currency_type
 
-currency1 = Money(100.50, "usd")
-currency2 = Money(200.50, "eur")
-print(currency1 < currency2)
-print(currency1 + currency2)
+
+usd = Money(100.50, "USD")
+eur = Money(100.50, "eur")
+jpy = Money(100.50, "JPY")
+btc = Money(100.50, "btc")
+addition = usd + eur
+multiplication = usd * btc
+subtraction = jpy - eur
+
+print("USD == EUR: ", usd == eur)
+print("BTC != EUR: ", btc != eur)
+print("JPY <= EUR: ", jpy <= eur)
+print("USD < EUR: ", usd < eur)
+print("USD >= EUR: ", usd >= eur)
+print("USD > EUR: ", usd > eur)
+print("USD + EUR: ", addition)
+print("JPY - EUR: ", subtraction)
+print("USD * BTC: ", multiplication)
+print("BTC + EUR: ", btc + eur)
+
+# print(Money(100.00, "usd") + Money(56.32, "eur") + Money(1.2, "btc") + Money(8, "jpy"))
